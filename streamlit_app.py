64d530bb8f4e2b416945364a585e3fc6874afc9c
Yoon2project <140355884+Yoon2project@users.noreply.github.com>
2023-07-26 09:42:25 -0400

    Update streamlit_app.py
    add #import pandas and #import requests (when we removed the import commands to move to the line2 we felt a little disoriented,so we put them back in with #)

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
+#import pandas (when we removed the import commands to move to the line2 we felt a little disoriented,so we put them back in with #)
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -30,7 +30,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-
+#import requests(when we removed the import commands to move to the line2 we felt a little disoriented,so we put them back in with #)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 # take the json version of the response and normalize it
